7c28606ea0074bcbc5537f62804d7ccf3b37285b
jkuchar <jkuchar@hexmotive.com>
2012-12-06 00:03:44 +0100

    dependent restrict costumer,admin : orders

--- app/controllers/crud_customers_controller.rb
+++ app/controllers/crud_customers_controller.rb
@@ -63,7 +63,13 @@ class CrudCustomersController < ApplicationController
   # DELETE /customers/1.json
   def destroy
     @customer = Customer.find(params[:id])
-    @customer.destroy
+    
+
+    begin
+      @customer.destroy
+    rescue ActiveRecord::DeleteRestrictionError => e
+      flash[:alert] = "Cannot delete dependent orders exist"
+    end
 
     respond_to do |format|
       format.html { redirect_to customers_url }
